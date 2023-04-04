7fbdd3ef29eb99c0b829a85aa8aa5d6639895642
Phillip Djurdjevic <a1i3n37@outlook.com>
2023-04-04 23:30:01 +0200

    Removed exception in LogManager for better testability.

--- WPF/ModularRxTemplate/SampleApp.Common/Utilities/LogManager.cs
+++ WPF/ModularRxTemplate/SampleApp.Common/Utilities/LogManager.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Reactive.Disposables;
 using Microsoft.Extensions.Logging;
 
 namespace SampleApp.Common.Utilities;
 
 public class LogManager
 {
-    private const string LoggerFactoryNotProvided = "Logger factory has not been providede yet.";
     private static readonly Lazy<LogManager> LazyInstance = new(() => new LogManager());
+    private static readonly Lazy<NoLogger> LazyNoLogger = new(() => new NoLogger());
 
     public static LogManager Current => LazyInstance.Value;
     
@@ -16,6 +17,13 @@ public class LogManager
     {
     }
 
-    public static ILogger GetLogger<T>() => Current.Factory?.CreateLogger<T>() ?? throw new InvalidOperationException(LoggerFactoryNotProvided);
-    public static ILogger GetLogger(string loggerName) => Current.Factory?.CreateLogger(loggerName) ?? throw new InvalidOperationException(LoggerFactoryNotProvided);
+    public static ILogger GetLogger<T>() => (ILogger?)Current.Factory?.CreateLogger<T>() ?? LazyNoLogger.Value;
+    public static ILogger GetLogger(string loggerName) => Current.Factory?.CreateLogger(loggerName) ?? LazyNoLogger.Value;
+
+    private class NoLogger : ILogger
+    {
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) { }
+        public bool IsEnabled(LogLevel logLevel) => false;
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull => new CompositeDisposable();
+    }
 }
